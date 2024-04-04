4658f88e9ce7b61f44d00c2dea06042b24ecc247
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-04 12:26:04 +0200

    Add project files.

---
+++ Classlib/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Classlib
+{
+    public class Solution
+    {
+        public int MaxDepth(string s)
+        {
+            int max = 0;
+            int count = 0;
+            foreach (char c in s)
+            {
+                if (c == '(')
+                {
+                    count++;
+                    max = count > max ? count : max;
+                }
+                else if (c == ')')
+                {
+                    count--;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Maximum Nesting Depth of the Parentheses/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classlib;
+namespace Maximum_Nesting_Depth_of_the_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            string s = "(1+(2*3)+((8)/4))+1";
+            System.Console.WriteLine(sol.MaxDepth(s));
+        }
+    }
+}
